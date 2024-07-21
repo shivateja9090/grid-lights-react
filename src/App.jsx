ccb3aada71de575ec75675e226e828a585c45f24
gajendrula-shiva <shivateja@vassarlabs.com>
2024-07-21 22:12:55 +0530

    css fix

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import { useState,useEffect } from "react";
 import "./App.css";
-
+import "./index.css"
 function App() {
   const [active ,setActive ] = useState([])
   const handleClick = (item) => {
